a65bf74094d21089c60b1e0587b5844ac9bf46ae
Vectormike40 <victor.junior40@yahoo.com>
2019-07-03 10:30:00 +0100

    Post a tweet

--- dist/bot.js
+++ dist/bot.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var _twit = _interopRequireDefault(require("twit"));
+
+var _config = _interopRequireDefault(require("./config"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var bot = new _twit["default"](_config["default"]);
--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,15 @@
 import Twit from 'twit';
 import config from './config';
+
 const bot = new Twit(config);
 
 
+// Post a tweet
+bot.post('statuses/update', {status: 'bot working 👎'}, (err, data, response) => {
+    if (err) {
+        console.log(err);
+    } else { 
+        console.log(`Tweet =>: ${data.text} `)
+    }
+    console.log(data);
+})
