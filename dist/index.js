a53dd5ce1d20312472887fd7b2f0abf9ce8220b6
Vectormike40 <victor.junior40@yahoo.com>
2019-07-03 10:30:37 +0100

    ...

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,3 @@
 "use strict";
 
-var _twit = _interopRequireDefault(require("twit"));
-
 require("./bot");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
