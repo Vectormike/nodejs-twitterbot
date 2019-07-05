86cc79888b39620cbdacb7ae959bfc01001e90e4
vectormike40 <victorjonah199@gmail.com>
2019-07-05 16:03:02 +0100

    cleanup

--- src/bot.js
+++ src/bot.js
@@ -1,40 +1,32 @@
-import config from './config';
-import Twit from 'twit';
-const bot = new Twit(config.key);
+const twit = require("twit");
+const config = require("./config");
+const bot = new twit(config.key);
 
+
+//Api
+const favorite = require("./api/favorite");
+
+favorite();
 // import tweet from './api/tweet';
 
 
 
 // Like a tweet
 
-let params = {
-    q: '#reactjs',
-    result_type: 'recent',
-    lang: 'en',
-    count: 1
-};
+// let params = {
+//     q: '#reactjs',
+//     result_type: 'recent',
+//     lang: 'en',
+//     count: 1
+// };
 
-bot.get('search/tweets', params, 
-    (err, data) => {
-        let tweet = data
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(data.statuses)
-        }
-    }
-)
-
-// bot.post('favorites/create', 
-//     {
-//         id: '974'
-//     },
-//     (err, data, response) => {
+// bot.get('search/tweets', params, 
+//     (err, data) => {
+//         let tweet = data
 //         if (err) {
-//             console.log(err);
+//             console.log(err)
 //         } else {
-//             console.log(`I just liked => ${data.text}`)
+//             console.log(data.statuses)
 //         }
-//     }    
+//     }
 // )
--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 
+// export const key = {
+//   consumer_key: process.env.CONSUMER_KEY,
+//   consumer_secret: process.env.CONSUMER_SECRET,
+//   access_token: process.env.ACCESS_TOKEN,
+//   access_token_secret: process.env.ACCESS_TOKEN_SECRET
+// };
+
 module.exports = {
-  key: {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  }
+    key: {
+        consumer_key: "AitQOm5y3ihVoIjgNt29Z67K5",
+        consumer_secret: "TBEmwtt35SmMa52xOxbSjql1kOx2URUDzVymMVoOyZvWd8mrvq",
+        access_token: "1145973212326629376-anSOnUdJ0o2EcMVFvckPF1qAYVRZ1u",
+        access_token_secret: "K4eqP8dRxVNa75yCs8QgIK2helLtAQNo74ktU0QSRCVV3"  
+    }
+   
 }
