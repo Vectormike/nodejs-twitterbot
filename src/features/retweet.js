031faa211c2968f25e4056039bb5794e1cf207f9
vectormike40 <victorjonah199@gmail.com>
2019-07-07 11:50:11 +0100

    Retweeting

--- src/features/retweet.js
+++ src/features/retweet.js
@@ -3,20 +3,45 @@ const config = require('../config');
 const bot = new twit(config.key);
 const random = require('./random.js');
 
+const retweet = () => {
 
-// Search specific tweets
-bot.get('search/tweets', {
-        q: '@vectormike_',
-        count: 5
-    }, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data.statuses);
+    let params = {
+		q: '#Reactjs, #javaScript, #Nodejs, #express, #Redux, #MERN',
+		result_type: 'recent',
+		lang: 'en'
+	}
+
+    // Search specific tweets
+    bot.get('search/tweets', params,
+        (err, data) => {
+
+            let tweets = data.statuses;
+            
+            // Pick a random tweet
+    		let tweet = random(tweets);
+	    	console.log(tweet.id_str);
+
+            if (err) {
+                console.log(err);
+            } else {
+            
+            // Retweet
+            bot.post('statuses/retweet/:id', {
+                    id: tweet.id_str
+                }, (err, data) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(`I just retweeted => ${data.text}`)
+                    }
+                }
+            )}
         }
-    }
+    )
+   
+}
 
-)
+module.exports = retweet;
 
 // Search specific tweets
 // Pick at random
