8eb89f070c4f76fb45687a8fa5d8082f874672b5
vectormike40 <victorjonah199@gmail.com>
2019-07-07 11:49:50 +0100

    Setinterval for retweet

--- src/bot.js
+++ src/bot.js
@@ -1,32 +1,8 @@
-const twit = require("twit");
-const config = require("./config");
-const bot = new twit(config.key);
-
 
 //Api
 const favorite = require("./features/favorite");
 setInterval(favorite, 100000)
 
+const retweet = require('./features/retweet');
+setInterval(retweet, 100000)
 
-
-
-
-// Like a tweet
-
-// let params = {
-//     q: '#reactjs',
-//     result_type: 'recent',
-//     lang: 'en',
-//     count: 1
-// };
-
-// bot.get('search/tweets', params, 
-//     (err, data) => {
-//         let tweet = data
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             console.log(data.statuses)
-//         }
-//     }
-// )
