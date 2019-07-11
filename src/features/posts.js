fe8c3e5594d3e57386236d7cd610b8f6cc131f96
vectormike40 <victorjonah199@gmail.com>
2019-07-11 06:38:26 +0100

    Tested tweet

--- src/features/posts.js
+++ src/features/posts.js
@@ -1,12 +1,17 @@
 const twit = require('twit');
 const config = require('../config');
 const bot = new twit(config.key);
+const random = require('./random');
+const status = require('./status');
 
 const posts = () => {
 
+        // Select posts at random
+        let tweet = random(status)
+
         // Post status from a an array
         bot.post('statuses/update', 
-        {status: 'Hello'}, 
+        { status: tweet }, 
             (err, data) => {
             if(err) {
                 console.log(err)
