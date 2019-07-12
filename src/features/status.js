5311bad945634977c3e4449ca0105c9ffa59285d
vectormike40 <victorjonah199@gmail.com>
2019-07-12 10:35:18 +0100

    More

--- src/features/status.js
+++ src/features/status.js
@@ -1,3 +1,6 @@
 module.exports = [
-    'A new day, another opportunity to be world class 🔥🚀'
+    'A new day, another opportunity to be world class 🔥🚀.',
+    'A clever person solves a problem. A wise person AVOIDS it. Pen down that this morning.',
+    'Good morning! Just a little reminder to YOU that anything is possible, especially when you’ve hit a wall with your code and feel like you’re getting nowhere. 👍',
+    'Your ideas are probably worth more than you think they are 👍.'
 ]
