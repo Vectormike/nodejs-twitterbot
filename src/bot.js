d648f48862a0b76b7a6a845f46a340eccf488c55
vectormike40 <victorjonah199@gmail.com>
2019-07-20 12:37:20 +0100

    Little change

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,6 @@
-
+const twit = require('twit');
+const config = require('./config');
+const bot = new twit(config.key)
 //Api
 const favorite = require("./features/favorite");
 setInterval(favorite, 100000)
@@ -9,5 +11,7 @@ setInterval(retweet, 1000000)
 const post = require('./features/posts');
 setInterval(post, 1000 * 60 * 60 * 24);
 
-const follow = require('./features/follow')
-follow()
+// const followed = require('./features/follow');
+
+// const stream = bot.stream('user');
+// stream.on('follow', followed)
