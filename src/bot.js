37d97c885c805e76d29623f91f212871bb44a62c
vectormike40 <victorjonah199@gmail.com>
2019-07-20 12:46:38 +0100

    (Unfinished)

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,19 @@
-const twit = require('twit');
-const config = require('./config');
-const bot = new twit(config.key)
+const twit = require("twit");
+const config = require("./config");
+const bot = new twit(config.key);
 //Api
 const favorite = require("./features/favorite");
-setInterval(favorite, 100000)
+setInterval(favorite, 100000);
 
-const retweet = require('./features/retweet');
-setInterval(retweet, 1000000)
+const retweet = require("./features/retweet");
+setInterval(retweet, 1000000);
 
-const post = require('./features/posts');
+const post = require("./features/posts");
 setInterval(post, 1000 * 60 * 60 * 24);
+
+// const autodm = require("./features/autodm");
+// autodm();
+
 
 // const followed = require('./features/follow');
 
