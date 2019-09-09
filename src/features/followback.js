f1f63aa4aba6bb6a6021b09c7e2dba756b107c72
Vectormike <victorjonah199@gmail.com>
2019-09-09 14:21:22 +0100

    Follow user init

--- src/features/followback.js
+++ src/features/followback.js
@@ -1,22 +1,40 @@
 const twit = require("twit");
 const config = require("../config");
 const bot = new twit(config.key);
+const random = require("./random");
 
 const followback = () => {
   bot.get(
-    "followers/ids",
+    "followers/list",
     {
       screen_name: "Vectorbottress"
     },
     (err, data) => {
-      const followers = data.ids;
       if (err) {
-        console.log(`Error is ${err}`);
+        console.log(err.message);
       } else {
-        console.log(followers);
+        const follow = data.users.map(user => user.screen_name);
+        let user = random(follow);
+
+        //      const follow = data.users.map(user => user.screen_name);
+        // Pick a random tweet
+        // let user = random(follow);
+
+        bot.post(
+          "friendships/create",
+          {
+            screen_name: user
+          },
+          (err, data) => {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log(data.following);
+            }
+          }
+        );
       }
-    },
-    bot.get('friendship/create', {})
+    }
   );
 };
 
